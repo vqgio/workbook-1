0fc34874e27b5d5ae407af1cffc579e3121418c8
Gio <giovannizavala82@gmail.com>
2025-04-09 10:26:58 -0500

    completed ShoppingList app

---
+++ first-maven-project1/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("hello World");
+
+    }
+}
---
+++ first-maven-project1/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Strawberry, mango, milk, eggs, bread, butter, carrots, avocado, chicken, steak");
+        
+    }
+
+}
