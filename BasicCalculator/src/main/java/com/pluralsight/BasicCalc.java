76a13b27f9f8400aafe4d1e839785586c2fcd4ed
Gio <giovannizavala82@gmail.com>
2025-04-11 10:56:01 -0500

    Exercise 3 Step 1

---
+++ BasicCalculator/src/main/java/com/pluralsight/BasicCalc.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class BasicCalc {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter a second number: ");
+        int num2 = scan.nextInt();
+
+        int total = num1 + num2;
+
+        System.out.print("Total is: " + total);
+
+
+
+    }
+
+
+}
